557ba73a98ac29029de4e3659a945119ac7d403e
anzwkk <anam89737@gmail.com>
2025-01-31 23:46:14 +0300

    Implemented file copy functionality with user input

--- 16.1HW/Program.cs
+++ 16.1HW/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿//мета - створити програму, яка копіює вміст одного текстового файлу в інший. Користувач повинен ввести шлях
+//до вихідного файлу та шлях до файлу, в який потрібно скопіювати дані. Після виконання копіювання виведіть повідомлення про успішне завершення.
+
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+
+        try
+        {
+
+            Console.Write("Введіть шлях до вихідного файлу: ");
+            string sourcePath = Console.ReadLine();
+
+            Console.Write("Введіть шлях до файлу, в який потрібно скопіювати дані: ");
+            string destinationPath = Console.ReadLine();
+
+            if (File.Exists(sourcePath))
+            {
+                File.Copy(sourcePath, destinationPath, true);
+                Console.WriteLine("Файл успішно скопійовано!");
+            }
+            else
+            {
+                Console.WriteLine("Вихідний файл не знайдено");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Сталася помилка: {ex.Message}");
+        }
+    }
+}
